2ebdc082be304f709e220b3fa9a93db0f3b5d570
drys262 <koeichavez17@gmail.com>
2019-05-07 18:36:14 +0800

    Adding some text in App.js && adding the cloudbuild.yaml file for the CI/CD Pipeline

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        React + Firebase Hosting + GCP Cloud Build
+        React + Firebase Hosting + GCP Cloud Build then Applying the GCP Cloud Build CI/CD Pipeline
       </header>
     </div>
   );
